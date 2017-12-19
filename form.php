3ab600c5ef0179a184f7e383cc457885fb66433f
Quinn Brown <PRTZLnet@users.noreply.github.com>
2017-12-18 23:00:04 -0500

    Add files via upload

--- form.php
+++ form.php
@@ -44,21 +44,38 @@
 					<a href="#" class="image fit"></a>
                     
                      
-<form method="post" action="process.php">
-<label>Name</label>
-<input placeholder="knyvez" type="text" name="name" />
-	<br />
-<label>Soundcloud</label>
-<input type="file" name="sc" />
-<br />
-	<br />
-<label>Email</label>
-<input placeholder="andrew@knyvez.com" type="text" name="email" />
- 
-<br />
-<input type="submit" value="Submit">
-</form>
+<?php
 
+ require_once 'google/appengine/api/cloud_storage/CloudStorageTools.php';
+ use google\appengine\api\cloud_storage\CloudStorageTools;
+ $options = [ 'gs_bucket_name' => 'cdn.prtzl.net' ];
+ $upload_url = CloudStorageTools::createUploadUrl('/form.php', $options);
+   if(isset($_POST['do-upload']) AND $_POST['do-upload'] === "yes"){
+   $yesupload = $_POST['do-upload'];
+   preg_match("/yes/", "".$yesupload."");
+
+   $filename = $_POST['name'] . '-' . $_FILES['testupload']['name'];
+
+   $gs_name = $_FILES['testupload']['tmp_name'];
+   move_uploaded_file($gs_name, 'gs://cdn.prtzl.net/'.$filename.'');
+?>
+
+<div class="contentArea">
+<?php
+echo "<b>Application sent!</p>";
+}
+
+echo"</div>";
+?>
+
+<form class="SomeSpaceDude" action="<?php echo $upload_url?>" enctype="multipart/form-data" method="post">
+<p>Files to upload: </p> <br>
+<input type="text" name="name"><br>
+ <input type="hidden" name="do-upload" value="yes">
+ <input class="SomeSpaceDude topcoat-button" type="file" name="testupload" >
+ <input class="SomeSpaceDude topcoat-button" type="submit" value="Upload">
+</form>
+</div>
 
 </body>
                     
